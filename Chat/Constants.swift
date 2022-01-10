43a9611aa4b4347922e1ab436bb77f52dbec2a41
Jose Carlos <josealarconchacon@gmail.com>
2022-01-09 20:20:34 -0500

    create new file to store app name and identifier

--- Chat/Constants.swift
+++ Chat/Constants.swift
@@ -8,6 +8,23 @@
 import Foundation
 
 struct Constants {
+    static let appName = "💬 Welcome to Chat"
+    static let cellIdentifier = "ReusableCell"
+    static let cellNibName = "MessageCell"
     static let registerSegue = "CreateAccountToChat"
     static let loginSegue = "LoginToChat"
+    
+    struct BrandColors {
+        static let purple = "BrandPurple"
+        static let lightPurple = "BrandLightPurple"
+        static let blue = "BrandBlue"
+        static let lighBlue = "BrandLightBlue"
+    }
+    
+    struct FStore {
+        static let collectionName = "messages"
+        static let senderField = "sender"
+        static let bodyField = "body"
+        static let dateField = "date"
+    }
 }
