b3cc2a9b65f18dc90f8412e63e5d8af839b582cf
Jose Carlos <josealarconchacon@gmail.com>
2022-01-09 20:55:01 -0500

    steup tableview datasource

--- Chat/Controller/MessageViewController.swift
+++ Chat/Controller/MessageViewController.swift
@@ -13,9 +13,16 @@ class MessageViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextfield: UITextField!
     
+    var messages: [Message] = [
+        Message(sender: "test@.com", body: "Hey!"),
+        Message(sender: "a@.com", body: "What up?"),
+        Message(sender: "test@.com", body: "How are you?")
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        title = "Chat App"
+        tableView.dataSource = self
+        title = Constants.appName
         navigationItem.hidesBackButton = true
     }
     
@@ -28,5 +35,17 @@ class MessageViewController: UIViewController {
         } catch let sigOutError as NSError {
             print("Error Signing Out: %@", sigOutError)
         }
+    }
+}
+
+extension MessageViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return messages.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: Constants.cellIdentifier, for: indexPath)
+        cell.textLabel?.text = messages[indexPath.row].body
+        return cell
     }
 }
---
+++ Chat/Model/Message.swift
@@ -0,0 +1,8 @@
+//
+//  Message.swift
+//  Chat
+//
+//  Created by Jose Alarcon Chacon on 1/9/22.
+//
+
+import Foundation
