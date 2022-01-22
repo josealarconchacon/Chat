c4a6119c483a5d88d681f8d9bccc8d062b8c2873
Jose Carlos <josealarconchacon@gmail.com>
2022-01-22 16:26:48 -0500

    set up logout button

--- Chat/Controller/ProfileViewController.swift
+++ Chat/Controller/ProfileViewController.swift
@@ -6,14 +6,28 @@
 //
 
 import UIKit
+import Firebase
 
 class ProfileViewController: UIViewController {
 
     @IBOutlet weak var signOutButton: UIButton!
+    @IBOutlet weak var userImage: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         signOutButton.layer.cornerRadius = 25.0
     }
     @IBAction func signOutPressed(_ sender: UIButton) {
+        signOut()
+    }
+}
+
+extension ProfileViewController {
+    func signOut() {
+        do {
+            try Auth.auth().signOut()
+            navigationController?.popToRootViewController(animated: true)
+        } catch let sigOutError as NSError {
+            print("Error Signing Out: %@", sigOutError)
+        }
     }
 }
